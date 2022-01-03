3612dfdae3a87e519800304c20aea05d5e9c1817
syauqeesy <syauqeesy@gmail.com>
2022-01-03 19:36:39 +0700

    More on Strings

--- javascript-complete-guide/basics-01-starting-project/assets/scripts/app.js
+++ javascript-complete-guide/basics-01-starting-project/assets/scripts/app.js
@@ -3,6 +3,8 @@ let currentResult = defaultResult
 
 currentResult = (currentResult + 10) * 3 / 2 - 1
 
-let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1'
+let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`
+let errorMessage = 'An error ' +
+'occurred'
 
 outputResult(currentResult, calculationDescription)
