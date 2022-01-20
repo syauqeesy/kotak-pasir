aa76b4a1085929133ed2ebdedd11275dd40d6e09
syauqeesy <syauqeesy@gmail.com>
2022-01-20 17:55:58 +0700

    Returning Values

--- javascript-complete-guide/basics/basics-01-starting-project/assets/scripts/app.js
+++ javascript-complete-guide/basics/basics-01-starting-project/assets/scripts/app.js
@@ -3,16 +3,11 @@ let currentResult = defaultResult
 
 function add (num1, num2) {
   const result = num1 + num2
-  alert('The result is ' + result)
+  return result
 }
 
-add(1, 2)
-add(5, 5)
-
-currentResult = (currentResult + 10) * 3 / 2 - 1
+currentResult = add(1, 2)
 
 let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`
-let errorMessage = 'An error ' +
-'occurred'
 
 outputResult(currentResult, calculationDescription)
