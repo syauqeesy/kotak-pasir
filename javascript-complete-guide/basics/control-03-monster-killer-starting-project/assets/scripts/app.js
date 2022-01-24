e591878f9211734dd8a2bbad3897854f4dd804be
syauqeesy <syauqeesy@gmail.com>
2022-01-24 17:53:44 +0700

    The "while" & "do-while" Loops

--- javascript-complete-guide/basics/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ javascript-complete-guide/basics/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -178,13 +178,23 @@ function healPlayerHandler () {
 }
 
 function printLogHandler() {
-  for (let i = 0; i < 3; i++) {
-    console.log('------')
-  }
-  for (let i = 10; i > 0;) {
-    i--
-    console.log(i)
-  }
+  // for (let i = 0; i < 3; i++) {
+  //   console.log('------')
+  // }
+  // for (let i = 10; i > 0;) {
+  //   i--
+  //   console.log(i)
+  // }
+
+  let j = 3
+  do {
+    console.log(j)
+    j++
+  } while (j < 3) 
+  // while (j < 3) {
+  //   console.log(j)
+  //   j++
+  // }
 
   // for (let i = 0; i < battleLog.length; i++) {
   //   console.log(battleLog[i])
