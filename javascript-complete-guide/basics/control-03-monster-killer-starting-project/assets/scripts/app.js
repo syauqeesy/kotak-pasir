fea017bfa079159caac6dc1a175efc2511733770
syauqeesy <syauqeesy@gmail.com>
2022-01-24 16:04:30 +0700

    Adding a "Reset Game" Functionality

--- javascript-complete-guide/basics/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ javascript-complete-guide/basics/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -10,6 +10,12 @@ let hasBonusLife = true
 
 adjustHealthBars(chosenMaxLife)
 
+function reset() {
+  currentMonsterHealth = chosenMaxLife
+  currentPlayerHealth = chosenMaxLife
+  resetGame(chosenMaxLife)
+}
+
 function endRound () {
   const initialPlayerHealth = currentPlayerHealth
   const playerDamage = dealPlayerDamage(monsterAttackValue)
@@ -28,6 +34,10 @@ function endRound () {
     alert('You lost!')
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
     alert('You have a draw!')
+  }
+
+  if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0) {
+    reset()
   }
 }
 
