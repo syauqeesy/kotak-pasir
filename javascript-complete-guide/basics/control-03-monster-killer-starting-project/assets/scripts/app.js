329811a43622d4f24b5f51263ac04e54d996b3c0
syauqeesy <syauqeesy@gmail.com>
2022-01-24 17:12:14 +0700

    Working with the "switch-case" Statement

--- javascript-complete-guide/basics/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ javascript-complete-guide/basics/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -34,34 +34,70 @@ function writeToLog(event, value, monsterHealth, playerHealth) {
     finalMonsterHealth: monsterHealth,
     finalPlayerHealth: playerHealth
   }
-  if (event === LOG_EVENT_PLAYER_ATTACK) {
-    logEntry.target = 'MONSTER'
-  } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-    logEntry.target = 'MONSTER'
-  } else if (event === LOG_EVENT_MONSTER_ATTACK) {
-    logEntry = {
-      event,
-      value,
-      target: 'PLAYER',
-      finalMonsterHealth: monsterHealth,
-      finalPlayerHealth: playerHealth
-    }
-  } else if (event === LOG_EVENT_PLAYER_HEAL) {
-    logEntry = {
-      event,
-      value,
-      target: 'PLAYER',
-      finalMonsterHealth: monsterHealth,
-      finalPlayerHealth: playerHealth
-    }
-  } else if (event === LOG_EVENT_GAME_OVER) {
-    logEntry = {
-      event,
-      value,
-      finalMonsterHealth: monsterHealth,
-      finalPlayerHealth: playerHealth
-    }
+  switch (event) {
+    case LOG_EVENT_PLAYER_ATTACK:
+      logEntry.target = 'MONSTER'
+      break
+    case LOG_EVENT_PLAYER_STRONG_ATTACK:
+      logEntry.target = 'MONSTER'
+      break
+    case LOG_EVENT_MONSTER_ATTACK:
+      logEntry = {
+        event,
+        value,
+        target: 'PLAYER',
+        finalMonsterHealth: monsterHealth,
+        finalPlayerHealth: playerHealth
+      }
+      break
+    case LOG_EVENT_PLAYER_HEAL:
+      logEntry = {
+        event,
+        value,
+        target: 'PLAYER',
+        finalMonsterHealth: monsterHealth,
+        finalPlayerHealth: playerHealth
+      }
+      break
+    case LOG_EVENT_GAME_OVER:
+      logEntry = {
+        event,
+        value,
+        finalMonsterHealth: monsterHealth,
+        finalPlayerHealth: playerHealth
+      }
+      break
+    default:
+      logEntry = {}
   }
+  // if (event === LOG_EVENT_PLAYER_ATTACK) {
+  //   logEntry.target = 'MONSTER'
+  // } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+  //   logEntry.target = 'MONSTER'
+  // } else if (event === LOG_EVENT_MONSTER_ATTACK) {
+  //   logEntry = {
+  //     event,
+  //     value,
+  //     target: 'PLAYER',
+  //     finalMonsterHealth: monsterHealth,
+  //     finalPlayerHealth: playerHealth
+  //   }
+  // } else if (event === LOG_EVENT_PLAYER_HEAL) {
+  //   logEntry = {
+  //     event,
+  //     value,
+  //     target: 'PLAYER',
+  //     finalMonsterHealth: monsterHealth,
+  //     finalPlayerHealth: playerHealth
+  //   }
+  // } else if (event === LOG_EVENT_GAME_OVER) {
+  //   logEntry = {
+  //     event,
+  //     value,
+  //     finalMonsterHealth: monsterHealth,
+  //     finalPlayerHealth: playerHealth
+  //   }
+  // }
 
   battleLog.push(logEntry)
 }
