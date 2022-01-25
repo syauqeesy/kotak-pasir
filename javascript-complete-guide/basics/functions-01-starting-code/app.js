a5f41892056e2a89386eb67f94f2da5f12c751e0
syauqeesy <syauqeesy@gmail.com>
2022-01-25 13:12:43 +0700

    Function Expressions: Storing Functions in Variables

--- javascript-complete-guide/basics/functions-01-starting-code/app.js
+++ javascript-complete-guide/basics/functions-01-starting-code/app.js
@@ -1,6 +1,6 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame() {
+const start = function () {
   console.log('Game is starting...')
 }
 
@@ -12,6 +12,6 @@ function startGame() {
 
 // person.greet()
 
-console.dir(startGame)
+// console.dir(startGame)
 
-startGameBtn.addEventListener('click', startGame)
+startGameBtn.addEventListener('click', start)
