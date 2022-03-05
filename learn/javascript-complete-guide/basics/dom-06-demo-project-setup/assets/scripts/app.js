77b34584b8ffab88c0988a7daac6c81096171b2a
syauqeesy <syauqeesy@gmail.com>
2022-03-05 16:22:04 +0700

    Fetching and Validating User Input

--- learn/javascript-complete-guide/basics/dom-06-demo-project-setup/assets/scripts/app.js
+++ learn/javascript-complete-guide/basics/dom-06-demo-project-setup/assets/scripts/app.js
@@ -9,6 +9,9 @@ const startAddMovieButton = document.querySelector('header button')
 const backdrop = document.getElementById('backdrop')
 
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive')
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling
+const userInputs = addMovieModal.querySelectorAll('input')
+// const userInputs = addMovieModal.getElementsByTagName('input')
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle('visible')
@@ -19,7 +22,7 @@ const toggleMovieModal = () => {
   toggleBackdrop()
 }
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
   toggleMovieModal()
 }
 
@@ -27,6 +30,23 @@ const backdropClickHandler = () => {
   toggleMovieModal()
 }
 
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value
+  const imageUrlValue = userInputs[1].value
+  const ratingValue = userInputs[2].value
+
+  if (
+    titleValue.trim() === '' ||
+    imageUrlValue.trim() === '' ||
+    ratingValue.trim() === '' ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert('Please enter valid values (rating between 1 and 5)!')
+  }
+}
+
 startAddMovieButton.addEventListener('click', toggleMovieModal)
 backdrop.addEventListener('click', backdropClickHandler)
-cancelAddMovieButton.addEventListener('click', cancelAddMovie)
+cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler)
+confirmAddMovieButton.addEventListener('click', addMovieHandler)
