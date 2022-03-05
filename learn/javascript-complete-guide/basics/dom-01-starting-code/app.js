55848cd52388145e63037ab5200452fca3aa3eae
syauqeesy <syauqeesy@gmail.com>
2022-03-05 15:16:55 +0700

    Styling DOM Elements

--- learn/javascript-complete-guide/basics/dom-01-starting-code/app.js
+++ learn/javascript-complete-guide/basics/dom-01-starting-code/app.js
@@ -3,3 +3,21 @@ const ul = document.body.firstElementChild.nextElementSibling
 const li = ul.firstElementChild
 
 console.log(li)
+
+const section = document.querySelector('section')
+
+// section.style.backgroundColor = 'blue'
+section.className = 'red-bg'
+
+const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+  // if (section.className === 'red-bg visible') {
+  //   section.className = 'red-bg invisible'
+  // } else {
+  //   section.className = 'red-bg visible'
+  // }
+
+  // section.classList.toggle('visible')
+  section.classList.toggle('invisible')
+})
