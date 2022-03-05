f55ea01b29bfc8aca073ededd9b434ef5027e5d2
syauqeesy <syauqeesy@gmail.com>
2022-03-05 13:41:57 +0700

    Selecting Multiple Elements & Summary

--- learn/javascript-complete-guide/basics/dom-01-starting-code/app.js
+++ learn/javascript-complete-guide/basics/dom-01-starting-code/app.js
@@ -1,1 +1,17 @@
-console.dir(document)
+const h1 = document.getElementById('main-title')
+
+console.dir(h1)
+
+h1.textContent = 'mantap sekali'
+h1.style.color = 'red'
+h1.style.backgroundColor = 'black'
+
+const li = document.querySelector('li:last-of-type')
+
+li.textContent = li.textContent + 'mantap'
+
+const lists = document.querySelectorAll('li')
+
+for (const list of lists) {
+  console.dir(list)
+}
