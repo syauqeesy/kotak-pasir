63a12ce7b4f6d3202db4c790e19a5b501408cf61
syauqeesy <syauqeesy@gmail.com>
2022-03-06 10:12:43 +0700

    sort()ing and reverse()ing

--- learn/javascript-complete-guide/basics/arrays-01-starting-code/app.js
+++ learn/javascript-complete-guide/basics/arrays-01-starting-code/app.js
@@ -76,4 +76,15 @@ const taxAdjustedPrices = prices.map((price, index, prices) => {
   return priceObj
 })
 
-console.log(prices, taxAdjustedPrices)
+// console.log(prices, taxAdjustedPrices)
+
+const sortedPrices = prices.sort((a, b) => {
+  if (a > b) {
+    return 1
+  } else if (a === b) {
+    return 0
+  } else {
+    return -1
+  }
+})
+console.log(sortedPrices.reverse())
