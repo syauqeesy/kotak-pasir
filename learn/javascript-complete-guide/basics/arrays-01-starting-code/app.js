e9c57ff385d2548fc0629714f49e9766ebef41ee
syauqeesy <syauqeesy@gmail.com>
2022-03-06 10:05:40 +0700

    Alternative to for Loops: The forEach() Method

--- learn/javascript-complete-guide/basics/arrays-01-starting-code/app.js
+++ learn/javascript-complete-guide/basics/arrays-01-starting-code/app.js
@@ -8,62 +8,77 @@ console.log(numbers)
 // const yetMoreNumbers = Array.of(1, 2)
 // console.log(yetMoreNumbers)
 
-const listItems = document.querySelectorAll('li')
-console.log(listItems)
+// const listItems = document.querySelectorAll('li')
+// console.log(listItems)
 
-const moreNumbers = Array.from(listItems)
-console.log(moreNumbers)
+// const moreNumbers = Array.from(listItems)
+// console.log(moreNumbers)
 
-const hobbies = ['Cooking', 'Sport']
-const personalData = [19, 'Syauqi', { moreDetail: '' }]
-const analitycsData = [[1, 1.3, 3], [2, -3, 1.3]]
+// const hobbies = ['Cooking', 'Sport']
+// const personalData = [19, 'Syauqi', { moreDetail: '' }]
+// const analitycsData = [[1, 1.3, 3], [2, -3, 1.3]]
 
-for (const data of analitycsData) {
-  for (const dataPoint of data) {
-    console.log(dataPoint)
-  }
-}
+// for (const data of analitycsData) {
+//   for (const dataPoint of data) {
+//     console.log(dataPoint)
+//   }
+// }
 
-console.log(personalData[1])
+// console.log(personalData[1])
 
-hobbies.push('Reading')
-hobbies.unshift('Playing Guitar')
-const poppedValue = hobbies.pop()
-hobbies.shift()
+// hobbies.push('Reading')
+// hobbies.unshift('Playing Guitar')
+// const poppedValue = hobbies.pop()
+// hobbies.shift()
 
-console.log(hobbies)
-
-// hobbies[1] = 'Coding'
-// hobbies[5] = 'Reading'
-// console.log(hobbies, hobbies[4])
-
-// hobbies.splice(1, 0, 'Eat')
-// console.log(hobbies)
-// hobbies.splice(0)
 // console.log(hobbies)
 
-const testResults = [2, 0, -1, 9.3, 4, 9.3, -9]
-console.log(testResults.includes(0))
-// const storedResults = testResults.slice(0, 2)
-const storedResults = testResults.concat([3.99, 2])
-testResults.push(7)
-console.log(testResults, storedResults)
-console.log(testResults.lastIndexOf(9.3))
+// // hobbies[1] = 'Coding'
+// // hobbies[5] = 'Reading'
+// // console.log(hobbies, hobbies[4])
 
-const personData = [{ name: 'Syauqi' }, { name: 'Aziz' }]
-console.log(personData.indexOf({ name: 'Syauqi' }))
+// // hobbies.splice(1, 0, 'Eat')
+// // console.log(hobbies)
+// // hobbies.splice(0)
+// // console.log(hobbies)
 
-const syauqi = personData.find((person, index, persons) => {
-  return person.name === 'Syauqi'
+// const testResults = [2, 0, -1, 9.3, 4, 9.3, -9]
+// console.log(testResults.includes(0))
+// // const storedResults = testResults.slice(0, 2)
+// const storedResults = testResults.concat([3.99, 2])
+// testResults.push(7)
+// console.log(testResults, storedResults)
+// console.log(testResults.lastIndexOf(9.3))
+
+// const personData = [{ name: 'Syauqi' }, { name: 'Aziz' }]
+// console.log(personData.indexOf({ name: 'Syauqi' }))
+
+// const syauqi = personData.find((person, index, persons) => {
+//   return person.name === 'Syauqi'
+// })
+
+// syauqi.name = 'Anna'
+
+// console.log(personData)
+// console.log(syauqi)
+
+// const azizIndex = personData.findIndex((person, index, persons) => {
+//   return person.name === 'Aziz'
+// })
+
+// console.log(azizIndex)
+
+const prices = [10000, 15000, 7500]
+const tax = 0.19
+const taxAdjustedPrices = []
+
+// for (const price of prices) {
+//   taxAdjustedPrices.push(price * (1 + tax))
+// }
+
+prices.forEach((price, index, prices) => {
+  const priceObj = { index,  taxAdjustedPrice: price * (1 + tax)}
+  taxAdjustedPrices.push(priceObj)
 })
 
-syauqi.name = 'Anna'
-
-console.log(personData)
-console.log(syauqi)
-
-const azizIndex = personData.findIndex((person, index, persons) => {
-  return person.name === 'Aziz'
-})
-
-console.log(azizIndex)
+console.log(taxAdjustedPrices)
