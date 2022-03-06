c8e9e60695a91a02c9616f5a0e40623fb6f1cf5f
syauqeesy <syauqeesy@gmail.com>
2022-03-06 09:04:33 +0700

    Creating Arrays

---
+++ learn/javascript-complete-guide/basics/arrays-01-starting-code/app.js
@@ -0,0 +1,15 @@
+const numbers = [1, 2, 3]
+
+console.log(numbers)
+
+// const moreNumbers = new Array(5, 2)
+// console.log(moreNumbers)
+
+// const yetMoreNumbers = Array.of(1, 2)
+// console.log(yetMoreNumbers)
+
+const listItems = document.querySelectorAll('li')
+console.log(listItems)
+
+const moreNumbers = Array.from(listItems)
+console.log(moreNumbers)
