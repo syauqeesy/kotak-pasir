d3e5a828e1e6caa3e89e5d42d35efa378a65a38f
syauqeesy <syauqeesy@gmail.com>
2022-03-06 20:45:05 +0700

    Starting with the "Switch Project" Logic

--- learn/javascript-complete-guide/basics/practice-oop-01-starting-setup/assets/scripts/app.js
+++ learn/javascript-complete-guide/basics/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -1,11 +1,42 @@
 class Tooltip {}
 
-class ProjectItem {}
+class ProjectItem {
+  constructor (id) {
+    this.id = id
+    this.connectSwitchButton()
+    this.connectMoreInfoButton()
+  }
+
+  connectMoreInfoButton() {
+
+  }
+
+  connectSwitchButton () {
+    const projectItemElement = document.getElementById(this.id)
+    const switchButton = projectItemElement.querySelector('button:last-of-type')
+    switchButton.addEventListener('click', )
+  }
+}
 
 class ProjectList {
+  projects = []
+
   constructor (type) {
     const projectItems = document.querySelectorAll(`#${type}-projects li`)
-    console.log(projectItems)
+    for (const projectItem of projectItems) {
+      this.projects.push(new ProjectItem(projectItem.id))
+    }
+    console.log(this.projects)
+  }
+
+  addProject () {
+
+  }
+  
+  switchProject (projectId) {
+    // const projectIndex = this.projects.findIndex(project => project.id === projectId)
+    // this.projects.splice(projectIndex, 1)
+    this.projects = this.projects.filter(project => project.id !== projectId)
   }
 }
 
