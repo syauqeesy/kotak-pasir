eabc62ec4b01f6f15095b4670de8bbcd7f80183a
syauqeesy <syauqeesy@gmail.com>
2022-03-06 14:27:18 +0700

    The "this" Keyword And Its Strange Behavior

--- learn/javascript-complete-guide/basics/objects-01-starting-project/assets/scripts/objects.js
+++ learn/javascript-complete-guide/basics/objects-01-starting-project/assets/scripts/objects.js
@@ -50,8 +50,9 @@ const renderMovies = (filter = '') => {
     const { info, ...otherProps } = movie
     console.log(otherProps)
     // const { title: movieTitle } = info
-    // const { getFormattedTitle } = movie
-    let text = movie.getFormattedTitle() + ' - '
+    let { getFormattedTitle } = movie
+    getFormattedTitle = getFormattedTitle.bind(movie)
+    let text = getFormattedTitle() + ' - '
 
     for (const key in info) {
       if (key !== 'title') {
