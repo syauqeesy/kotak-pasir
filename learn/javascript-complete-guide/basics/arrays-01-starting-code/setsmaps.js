ff6780888f225a75aa19dbcb6f218ee8fb8bd0b1
syauqeesy <syauqeesy@gmail.com>
2022-03-06 11:02:56 +0700

    Working with Maps

--- learn/javascript-complete-guide/basics/arrays-01-starting-code/setsmaps.js
+++ learn/javascript-complete-guide/basics/arrays-01-starting-code/setsmaps.js
@@ -1,9 +1,32 @@
-const ids = new Set(['Hi', 'From', 'Set'])
-ids.add(2)
-if (ids.has('Hi'))
-  ids.delete('Hi')
+// const ids = new Set(['Hi', 'From', 'Set'])
+// ids.add(2)
+// if (ids.has('Hi'))
+//   ids.delete('Hi')
 
-for (const entry of ids.values()) {
-  console.log(entry)
+// for (const entry of ids.values()) {
+//   console.log(entry)
+// }
+// console.log(ids)
+
+const person1 = { name: 'Ahmad' }
+const person2 = { name: 'Aziz' }
+
+const personData = new Map([[person1, [{ date: 'yesterday', price: 10 }]]])
+console.log(personData.get(person1));
+
+personData.set(person2, [{ date: '2 weeks ago', price: 9 }])
+console.log(personData)
+
+for (const [key, value] of personData.entries()) {
+  console.log(key, value)
 }
-console.log(ids)
+
+for (const key of personData.keys()) {
+  console.log(key)
+}
+
+for (const value of personData.values()) {
+  console.log(value)
+}
+
+console.log(personData.size)
