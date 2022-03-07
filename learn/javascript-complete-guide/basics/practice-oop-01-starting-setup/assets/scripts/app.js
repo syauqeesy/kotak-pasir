5320cc1baa6db93359095f83a18faa83a2a3311d
syauqeesy <syauqeesy@gmail.com>
2022-03-07 20:44:05 +0700

    SettinG Timers & Intervals

--- learn/javascript-complete-guide/basics/practice-oop-01-starting-setup/assets/scripts/app.js
+++ learn/javascript-complete-guide/basics/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -156,9 +156,12 @@ class App {
     finishedProjectsList.setSwitchHandler(activeProjectsList.addProject.bind(activeProjectsList))
 
     const someScript = document.createElement('script')
-    someScript.textContent = 'alert("Hi there!")'
+    // someScript.textContent = 'alert("Hi there!")'
     document.head.append(someScript)
-    document.getElementById('start-analytics-btn').addEventListener('click', this.startAnalytics)
+    const timerId = setTimeout(this.startAnalytics, 3000)
+    document.getElementById('stop-analytics-btn').addEventListener('click', () => {
+      clearTimeout(timerId)
+    })
   }
 
   static startAnalytics () {
