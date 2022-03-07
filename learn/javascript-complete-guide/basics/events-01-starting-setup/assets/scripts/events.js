3102e4f6157e1800574367844578a3290d761ad5
syauqeesy <syauqeesy@gmail.com>
2022-03-07 21:11:48 +0700

    Different Ways of Listening to Events

---
+++ learn/javascript-complete-guide/basics/events-01-starting-setup/assets/scripts/events.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('button')
+
+// const buttonClickHandler = () => {
+//   alert('button was clicked')
+// }
+
+// const anotherButtonClickHandler = () => {
+//   alert('button was clicked 2')
+// }
+
+// button.onclick = buttonClickHandler
+
+button.addEventListener()
+// button.removeEventListener()
