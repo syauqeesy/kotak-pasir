950753807afc4fb246902fe7862d705f8cfba368
syauqeesy <syauqeesy@gmail.com>
2022-03-07 21:49:18 +0700

    Event Propagation & stopPropagation()

--- learn/javascript-complete-guide/basics/events-01-starting-setup/assets/scripts/events.js
+++ learn/javascript-complete-guide/basics/events-01-starting-setup/assets/scripts/events.js
@@ -1,4 +1,4 @@
-// const buttons = document.querySelectorAll('button')
+const button = document.querySelector('button')
 
 // const buttonClickHandler = event => {
 //   // event.target.disabled = true
@@ -27,5 +27,18 @@ const form = document.querySelector('form')
 
 form.addEventListener('submit', event => {
   event.preventDefault()
+  console.log(event)
+})
+
+const div = document.querySelector('div')
+
+div.addEventListener('click', event => {
+  console.log('CLICKED DIV')
+  console.log(event)
+})
+
+button.addEventListener('click', event => {
+  event.stopPropagation()
+  console.log('CLICKED BUTTON')
   console.log(event)
 })
