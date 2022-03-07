dbd8c3df9f28f89764f36fe1712bfad2757221b8
syauqeesy <syauqeesy@gmail.com>
2022-03-07 21:16:28 +0700

    Removing Event Listeners

--- learn/javascript-complete-guide/basics/events-01-starting-setup/assets/scripts/events.js
+++ learn/javascript-complete-guide/basics/events-01-starting-setup/assets/scripts/events.js
@@ -1,14 +1,17 @@
 const button = document.querySelector('button')
 
-// const buttonClickHandler = () => {
-//   alert('button was clicked')
-// }
+const buttonClickHandler = () => {
+  alert('button was clicked')
+}
 
-// const anotherButtonClickHandler = () => {
-//   alert('button was clicked 2')
-// }
+const anotherButtonClickHandler = () => {
+  alert('button was clicked 2')
+}
 
 // button.onclick = buttonClickHandler
 
-button.addEventListener()
-// button.removeEventListener()
+button.addEventListener('click', buttonClickHandler)
+
+setTimeout(() => {
+  button.removeEventListener('click', buttonClickHandler)
+}, 2000);
