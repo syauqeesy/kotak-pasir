4415c079848d51a22ae04f28f7d1ee67344a9e4f
syauqeesy <syauqeesy@gmail.com>
2022-03-08 19:10:45 +0700

    Tagged Templates

--- learn/javascript-complete-guide/basics/nums-strings-01-starting-setup/app.js
+++ learn/javascript-complete-guide/basics/nums-strings-01-starting-setup/app.js
@@ -3,3 +3,16 @@ function randomIntbetween(min, max) {
 }
 
 console.log(randomIntbetween(10, 20))
+
+function productDescription (strings, productName, productPrice) {
+  console.log(strings)
+  console.log(productName)
+  console.log(productPrice)
+  return 'This is a product!'
+}
+
+const prodName = 'JavaScript Course'
+const prodPrice = 29.99
+
+const productOutput = productDescription`This product (${prodName}) is ${prodPrice}`
+console.log(productOutput)
