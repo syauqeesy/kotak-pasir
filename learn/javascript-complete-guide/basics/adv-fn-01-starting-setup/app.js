dea45e28efedd56cc81a5113927830840468c6d8
syauqeesy <syauqeesy@gmail.com>
2022-03-08 16:54:29 +0700

    Introducing "Recursion"

--- learn/javascript-complete-guide/basics/adv-fn-01-starting-setup/app.js
+++ learn/javascript-complete-guide/basics/adv-fn-01-starting-setup/app.js
@@ -60,3 +60,24 @@ let name = 'Aziz'
 userName = 'Ahmed'
 
 greetUser()
+
+// function powerOf(x, n) {
+//   let result = 1
+
+//   for (let i = 0; i < n; i++) {
+//     result *= x
+//   }
+
+//   return result
+// }
+
+function powerOf(x, n) {
+  // if (n === 1) {
+  //   return x
+  // }
+
+  // return x * powerOf(x, n - 1)
+  return n === 1 ? x : x * powerOf(x, n - 1)
+}
+
+console.log(powerOf(2, 3)) // 2 * 2 * 2
