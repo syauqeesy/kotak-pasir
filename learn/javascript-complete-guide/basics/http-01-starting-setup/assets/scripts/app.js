b8341e0ae72e378fb869d856733b7503eb13e923
syauqeesy <syauqeesy@gmail.com>
2022-03-10 21:00:45 +0700

    Adding Request Headers

--- learn/javascript-complete-guide/basics/http-01-starting-setup/assets/scripts/app.js
+++ learn/javascript-complete-guide/basics/http-01-starting-setup/assets/scripts/app.js
@@ -6,7 +6,8 @@ const postList = document.querySelector('ul')
 
 function sendHttpRequest(method, url, data) {
   // const promise = new Promise((resolve, reject) => {
-  //   const xhr = new XMLHttpRequest()
+  // const xhr = new XMLHttpRequest()
+  // xhr.setRequestHeader('Content-Type', 'application/json')
   //   xhr.open(method, url)
   //   xhr.responseType = 'json'
   //   xhr.onload = function () {
@@ -23,7 +24,10 @@ function sendHttpRequest(method, url, data) {
 
   return fetch(url, {
     method,
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
   }).then(response => {
     return response.json()
   })
