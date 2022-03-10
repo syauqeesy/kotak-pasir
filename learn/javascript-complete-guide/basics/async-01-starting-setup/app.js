bd6bbc41bac48241a4ed7c21d1d259c0743a5ef1
syauqeesy <syauqeesy@gmail.com>
2022-03-10 19:02:02 +0700

    Getting Started with Promises

--- learn/javascript-complete-guide/basics/async-01-starting-setup/app.js
+++ learn/javascript-complete-guide/basics/async-01-starting-setup/app.js
@@ -1,18 +1,28 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+function setTimer (duration) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!')
+    }, duration)
+  })
+
+  return promise
+}
+
 function trackUserHandler() {
   navigator.geolocation.getCurrentPosition(posData => {
-    setTimeout(() => {
-      console.log(posData)
-    }, 2000)
+    setTimer(2000).then(data => {
+      console.log(data, posData)
+    })
   }, error => {
     console.log(error)
   })
 
-  setTimeout(() => {
+  setTimer(0).then(() => {
     console.log('Timer done')
-  }, 0)
+  })
   console.log('Getting position...')
 }
 
