0c5e5b8734a5796616c8b4011eec9d6e01eeaab6
syauqeesy <syauqeesy@gmail.com>
2022-03-10 19:33:05 +0700

    Async/ await & Error Handling

--- learn/javascript-complete-guide/basics/async-01-starting-setup/app.js
+++ learn/javascript-complete-guide/basics/async-01-starting-setup/app.js
@@ -25,8 +25,14 @@ async function setTimer (duration) {
 
 async function trackUserHandler() {
   // let positionData
-  const posData = await getPosition()
-  const timerData = await setTimer(2000)
+  let posData
+  let timerData
+  try {
+    posData = await getPosition()
+    timerData = await setTimer(2000)
+  } catch (error) {
+    console.log(error)
+  }
   console.log(posData, timerData)
     // .then(posData => {
     //   positionData = posData
