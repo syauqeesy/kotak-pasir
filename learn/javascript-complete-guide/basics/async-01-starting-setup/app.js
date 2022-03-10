92ff02b02aed1ec4191e5c5b0cce2d9e399e2bf3
syauqeesy <syauqeesy@gmail.com>
2022-03-10 19:15:58 +0700

    Promise Error Handling

--- learn/javascript-complete-guide/basics/async-01-starting-setup/app.js
+++ learn/javascript-complete-guide/basics/async-01-starting-setup/app.js
@@ -2,11 +2,11 @@ const button = document.querySelector('button');
 const output = document.querySelector('p');
 
 function getPosition (opts) {
-  const promise = new Promise((resolve, rejeect) => {
+  const promise = new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(success => {
       resolve(success)
     }, error => {
-
+      reject(error)
     }, opts)
   })
 
@@ -29,6 +29,10 @@ function trackUserHandler() {
     .then(posData => {
       positionData = posData
     return setTimer(2)
+    })
+    .catch(error => {
+      console.log(error)
+      return 'on we go..'
     })
     .then(data => {
       console.log(data, positionData)
