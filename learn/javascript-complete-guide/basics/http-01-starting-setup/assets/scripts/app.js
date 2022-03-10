83f5ef1e4b13698465600134a648d3850c7d94c3
syauqeesy <syauqeesy@gmail.com>
2022-03-10 20:13:57 +0700

    JSON Data & Parsing Data

--- learn/javascript-complete-guide/basics/http-01-starting-setup/assets/scripts/app.js
+++ learn/javascript-complete-guide/basics/http-01-starting-setup/assets/scripts/app.js
@@ -1,4 +1,18 @@
 const xhr = new XMLHttpRequest()
+const listElement = document.querySelector('.posts')
+const postTemplate = document.getElementById('single-post')
 
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts')
+xhr.responseType = 'json'
+xhr.onload = function () {
+  // const posts = JSON.parse(xhr.response)
+  const posts = xhr.response
+  for (const post of posts) {
+    const postElement = document.importNode(postTemplate.content, true)
+    postElement.querySelector('h2').textContent = post.title
+    postElement.querySelector('p').textContent = post.body
+    listElement.append(postElement)
+  }
+}
 xhr.send()
+
