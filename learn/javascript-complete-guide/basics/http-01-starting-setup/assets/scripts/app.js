935939657afa10bc43b4cfe4c2b6a5ff82b79c4e
syauqeesy <syauqeesy@gmail.com>
2022-03-10 20:40:23 +0700

    Sending a DELETE Request

--- learn/javascript-complete-guide/basics/http-01-starting-setup/assets/scripts/app.js
+++ learn/javascript-complete-guide/basics/http-01-starting-setup/assets/scripts/app.js
@@ -2,6 +2,7 @@ const listElement = document.querySelector('.posts')
 const postTemplate = document.getElementById('single-post')
 const form = document.querySelector('#new-post form')
 const fetchButton = document.querySelector('#available-posts button')
+const postList = document.querySelector('ul')
 
 function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
@@ -25,6 +26,7 @@ async function fetchPosts() {
     const postElement = document.importNode(postTemplate.content, true)
     postElement.querySelector('h2').textContent = post.title
     postElement.querySelector('p').textContent = post.body
+    postElement.querySelector('li').id = post.id
     listElement.append(postElement)
   }
 }
@@ -47,4 +49,11 @@ form.addEventListener('submit', event => {
   const enteredTitle = event.currentTarget.querySelector('#title').value
   const enteredBody = event.currentTarget.querySelector('#content').value
   createPost(enteredTitle, enteredBody)
+})
+
+postList.addEventListener('click', event => {
+  if (event.target.tagName === 'BUTTON') {
+    const postId = event.target.closest('li').id
+    sendHttpRequest('DELETE', `https://jsonplaceholder.typicode.com/posts/${postId}`)
+  }
 })
