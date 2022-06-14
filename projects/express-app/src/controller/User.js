2a810a09d226a3e6104f169342ddc8cb9e6f4cb1
behoon <syauqeesy@gmail.com>
2022-06-14 09:37:33 +0700

    Create register method

--- projects/express-app/src/controller/User.js
+++ projects/express-app/src/controller/User.js
@@ -1,5 +1,21 @@
 class User {
 
+  register(request, response) {
+    const responseBody = {
+      status: true,
+      code: 200,
+      message: 'Register success',
+      data: {
+        first_name: request.body.first_name,
+        last_name: request.body.last_name,
+        image: request.body.image,
+        username: request.body.username,
+        password: request.body.password
+      }
+    }
+
+    response.status(responseBody.code).json(responseBody)
+  }
 }
 
 module.exports = new User()
