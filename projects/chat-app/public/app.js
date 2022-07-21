2549063f4eb8bd9b1b69208d075fae36d5d6a734
behoon <syauqeesy@gmail.com>
2022-07-21 14:59:50 +0700

    Chat client example

--- projects/chat-app/public/app.js
+++ projects/chat-app/public/app.js
@@ -1,12 +1,23 @@
-const socket = io()
+const socket = io('localhost:5000')
 
 const form = document.getElementById('chat')
 const inputMessage = document.getElementById('message')
 
+socket.emit('join-room', {
+  room_id: 'room-id-1'
+})
+
 form.addEventListener('submit', event => {
   event.preventDefault()
 
-  socket.emit('send-message', {
-    message: inputMessage.value
+  socket.emit('send-message-to-server', {
+    message: inputMessage.value,
+    sender_id: 'user-id-1',
+    receiver_id: 'room-id-1',
+    created_at: new Date().toDateString()
   })
+})
+
+socket.on('send-message-to-receiver', data => {
+  console.log(data)
 })
--- projects/chat-app/src/index.js
+++ projects/chat-app/src/index.js
@@ -1,4 +1,3 @@
-const socket = require('socket.io')
 const express = require('express')
 const fs = require('fs')
 
@@ -24,12 +23,4 @@ app.get('/user', (request, response) => {
   response.send(users)
 })
 
-const server = app.listen(3000, () => console.log(`Server running on port 3000`))
-
-const io = socket(server)
-
-io.on('connection', socket => {
-  socket.on('send-message', arguments => {
-    console.log(arguments)
-  })
-})
+app.listen(3000, () => console.log(`Server running on port 3000`))
