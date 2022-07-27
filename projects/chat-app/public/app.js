56f9f1bf5a9819545e66544b4af7d790dc57ba6e
behoon <syauqeesy@gmail.com>
2022-07-27 16:30:13 +0700

    fix: Fix doubled data

--- projects/chat-app/public/app.js
+++ projects/chat-app/public/app.js
@@ -1,20 +1,20 @@
-const socket = io('localhost:5000')
+const socket = io('10.16.3.121:8010')
 
 const form = document.getElementById('chat')
 const inputMessage = document.getElementById('message')
 
 socket.emit('join-room', {
-  room_id: 'room-id-1'
+  room_id: 'aca18fac-a730-4959-9e00-ce667e2814e0'
 })
 
 form.addEventListener('submit', event => {
   event.preventDefault()
 
   socket.emit('send-message-to-server', {
-    message: inputMessage.value,
-    sender_id: 'user-id-1',
-    receiver_id: 'user-id-2',
-    created_at: new Date().toDateString()
+    chat_message: inputMessage.value,
+    chat_sender_id: '6abfafef-6b96-41e7-887c-9a26277b916f',
+    chat_receiver_id: 'CGKNRL',
+    chat_created_at: new Date().toISOString()
   })
 })
 
