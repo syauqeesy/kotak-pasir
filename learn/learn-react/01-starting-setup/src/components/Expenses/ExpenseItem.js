f5ff721426b9b34f8dbb71356c617d16c90cd303
behoon <syauqeesy@gmail.com>
2022-08-24 15:17:24 +0700

    rendering list of data

--- learn/learn-react/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ learn/learn-react/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -8,9 +8,9 @@ import './ExpenseItem.css'
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title)
 
-  const clickHandler = () => {
-    setTitle('Updated!')
-  }
+  // const clickHandler = () => {
+  //   setTitle('Updated!')
+  // }
 
   return (
     <Card className="expense-item">
@@ -19,7 +19,7 @@ const ExpenseItem = (props) => {
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   )
 }
