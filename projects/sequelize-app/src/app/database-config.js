c001231b805020cb8931deeda146fbbb111fd188
behoon <syauqeesy@gmail.com>
2022-08-24 18:57:15 +0700

    fix

--- projects/sequelize-app/src/app/database-config.js
+++ projects/sequelize-app/src/app/database-config.js
@@ -1,12 +1,23 @@
-const config = {
-  development: {
-    username: 'root',
-    password: 'sauki123',
-    database: 'sandbox',
-    host: '127.0.0.1',
-    port: 3306,
-    dialect: 'mysql'
+{
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   }
 }
-
-module.exports = config
