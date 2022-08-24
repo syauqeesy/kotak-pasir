721952d45ff34913df44ddd465540b22b1c2c7cb
behoon <syauqeesy@gmail.com>
2022-08-24 14:38:27 +0700

    handling form subsmission

--- learn/learn-react/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ learn/learn-react/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -43,8 +43,20 @@ const ExpenseForm = () => {
     // setUserInput((prevState) => ({ ...prevState, enteredDate: event.target.value }))
   }
 
+  const submitHandler = (event) => {
+    event.preventDefault()
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    }
+
+    console.log(expenseData)
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
