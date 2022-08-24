68204c8327cd875c62a615bcf1102fe76bdfb307
behoon <syauqeesy@gmail.com>
2022-08-24 16:22:08 +0700

    adding conditional return

---
+++ learn/learn-react/01-starting-setup/src/components/Expenses/ExpensesList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import ExpenseItem from './ExpenseItem'
+
+import './ExpensesList.css'
+
+const ExpensesList = (props) => {
+  if (props.expenses.length === 0) {
+    return <p className='expenses-list__fallback'>No expenses found.</p>
+  }
+
+  return (
+    <ul className='expenses-list'>
+      {props.expenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} /> )}
+    </ul>
+  )
+}
+
+export default ExpensesList
