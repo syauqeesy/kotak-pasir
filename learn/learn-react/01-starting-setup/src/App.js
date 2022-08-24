ed79d675f67db2b57cc388e61af9725e3a4ce7a4
behoon <syauqeesy@gmail.com>
2022-08-24 14:11:58 +0700

    adding form inputs

--- learn/learn-react/01-starting-setup/src/App.js
+++ learn/learn-react/01-starting-setup/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import Expense from './components/Expenses/Expenses'
+import NewExpense from './components/NewExpense/NewExpense'
 
 const App = () => {
   return (
     <div className="app">
+      <NewExpense />
       <Expense />
     </div>
   )
