2b270894fde0308c48bcd2d8feeaf45558ce2a81
behoon <syauqeesy@gmail.com>
2022-08-24 18:55:32 +0700

    dynamic inline styles

--- learn/learn-react/01-starting-setup/src/components/Expenses/Expenses.js
+++ learn/learn-react/01-starting-setup/src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 import ExpensesList from "./ExpensesList"
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
 import Card from "../UI/Card"
 
 import './Expenses.css'
@@ -21,6 +22,8 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+
+      <ExpensesChart expenses={filteredExpenses} />
 
       <ExpensesList expenses={filteredExpenses}/>
     </Card>
