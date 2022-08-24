2ec17673294d832e03fe92ee1318aa94ebfe6c83
behoon <syauqeesy@gmail.com>
2022-08-24 12:13:46 +0700

    dynamic data & expression in JSX

--- learn/learn-react/01-starting-setup/src/components/ExpenseItem.js
+++ learn/learn-react/01-starting-setup/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 2, 28)
+  const expenseTitle = 'Car Insurance'
+  const expenseAmount = 110.5
+
   return (
     <div className="expense-item">
-      <div>March 28th 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$110.5</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   )
