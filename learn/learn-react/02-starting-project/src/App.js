ae63db1ae9f84e607bdab6118fce4935f27bf238
behoon <syauqeesy@gmail.com>
2022-08-25 16:36:43 +0700

    users list component

--- learn/learn-react/02-starting-project/src/App.js
+++ learn/learn-react/02-starting-project/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import AddUser from './Users/AddUser';
+import UsersList from './Users/UsersList'
 
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList users={[]} />
     </div>
   );
 }
