ed70cb3aae7fbbd6ffee10e3ff8a99d563974cb5
behoon <syauqeesy@gmail.com>
2022-08-26 20:17:58 +0700

    adding cart reducer

--- learn/learn-react/07-starting-project/src/App.jsx
+++ learn/learn-react/07-starting-project/src/App.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import Header from "./components/Layout/Header";
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
-import cartProvider from './store/cartProvider';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false)
@@ -17,11 +17,11 @@ function App() {
   }
 
   return (
-    <cartProvider>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler}/>}
       <Header onShowCart={showCartHandler}></Header>
       <main><Meals /></main>
-    </cartProvider>
+    </CartProvider>
   );
 }
 
--- learn/learn-react/07-starting-project/src/store/cartProvider.jsx
+++ learn/learn-react/07-starting-project/src/store/cartProvider.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 
 import cartContext from "./cartContext"
 
-const cartProvider = props => {
-  const addItemToCartHandler = item => {}
-  const removeItemFromCartHandler = id => {}
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+  if(action.type === 'ADD_ITEM') {
+    const updatedItems = state.items.concat(action.item)
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+  }
+
+  return defaultCartState
+}
+
+const CartProvider = props => {
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
+
+  const addItemToCartHandler = item => {
+    dispatchCartAction({ type: 'ADD_ITEM', item })
+  }
+  const removeItemFromCartHandler = id => {
+    dispatchCartAction({ type: 'REMOVE_ITEM', id })
+  }
 
   const cartContextData = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler
   }
@@ -16,4 +41,4 @@ const cartProvider = props => {
   return <cartContext.Provider value={cartContextData}>{props.children}</cartContext.Provider>
 }
 
-export default cartProvider
+export default CartProvider
