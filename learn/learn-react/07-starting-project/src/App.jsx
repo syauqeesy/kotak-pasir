d076b18affb1c932597dc0255ec97ea10f198a6e
behoon <syauqeesy@gmail.com>
2022-08-26 15:54:45 +0700

    Meals component

--- learn/learn-react/07-starting-project/src/App.jsx
+++ learn/learn-react/07-starting-project/src/App.jsx
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react'
 
 import Header from "./components/Layout/Header";
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <Fragment>
       <Header></Header>
+      <main><Meals /></main>
     </Fragment>
   );
 }
