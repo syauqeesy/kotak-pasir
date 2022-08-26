c77d52f18ad8272786d30046f012f47a57225335
behoon <syauqeesy@gmail.com>
2022-08-26 13:40:34 +0700

    useEffect & dependencies

--- learn/learn-react/04-starting-project/src/App.js
+++ learn/learn-react/04-starting-project/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,10 +7,19 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem('isLoggedIn')
+
+    if (isLoggedIn === '1') {
+      setIsLoggedIn(true);
+    }
+  }, [])
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
+    localStorage.setItem('isLoggedIn', '1')
+    setIsLoggedIn(true)
   };
 
   const logoutHandler = () => {
