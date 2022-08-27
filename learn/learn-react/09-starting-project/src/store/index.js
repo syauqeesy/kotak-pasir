a9a1aac3d4368bbddbe80495a55b7d16aaf7acf7
behoon <syauqeesy@gmail.com>
2022-08-27 19:47:18 +0700

    use multiple slices

--- learn/learn-react/09-starting-project/src/store/index.js
+++ learn/learn-react/09-starting-project/src/store/index.js
@@ -21,10 +21,29 @@ const counterSlice = createSlice({
   }
 })
 
+const initialAuthenticationState = {
+  isAuthenticated: false
+}
+
+const authenticationSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthenticationState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true
+    },
+    logout(state) {
+      state.isAuthenticated = false
+    }
+  }
+})
+
 export const counterActions = counterSlice.actions
+export const authentication = authenticationSlice.actions
 
 export default configureStore({
   reducer: {
-    counter: counterSlice.reducer
+    counter: counterSlice.reducer,
+    authentication: authenticationSlice.reducer
   }
 })
