ea905a6ce5b134d74bbd5104f9506efb22f4c025
behoon <syauqeesy@gmail.com>
2022-08-27 20:00:28 +0700

    splitting redux store

---
+++ learn/learn-react/09-starting-project/src/store/authenticationSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialAuthenticationState = {
+  isAuthenticated: false
+}
+
+const authenticationSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthenticationState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true
+    },
+    logout(state) {
+      state.isAuthenticated = false
+    }
+  }
+})
+
+export default authenticationSlice
