138af82cc85b69e50828ad7097fc8e7ab12785de
behoon <syauqeesy@gmail.com>
2022-08-28 13:56:44 +0700

    using params

--- learn/learn-react/11-starting-project/src/pages/Products.jsx
+++ learn/learn-react/11-starting-project/src/pages/Products.jsx
@@ -1,5 +1,14 @@
 const Products = () => {
-  return <h1>Products Page</h1>
+  return (
+    <section>
+      <h1>Products Page</h1>
+      <ul>
+        <li>A Book</li>
+        <li>A Carpet</li>
+        <li>A Bread</li>
+      </ul>
+    </section>
+  )
 }
 
 export default Products
