553a948e4c8c77ab149705206ac39d2f7fd8e71a
behoon <syauqeesy@gmail.com>
2022-08-28 17:01:31 +0700

    setup new meetup form

--- learn/learn-react/06-onwards-to-a-bigger-project-starting-project/pages/new-meetup/index.js
+++ learn/learn-react/06-onwards-to-a-bigger-project-starting-project/pages/new-meetup/index.js
@@ -1,1 +1,13 @@
 // localhost:3000/new-meetup
+
+import NewMeetupForm from '../../components/meetups/NewMeetupForm'
+
+const NewMeetupPage = () => {
+  const addMeetupHandler = enteredMeetupData => {
+    console.log(enteredMeetupData)
+  }
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />
+}
+
+export default NewMeetupPage
