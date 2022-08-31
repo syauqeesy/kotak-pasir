0b4b74e2de3d74605084acdf4c493470394013f8
behoon <syauqeesy@gmail.com>
2022-08-31 10:49:57 +0700

    base

--- projects/category-app/pages/index.tsx
+++ projects/category-app/pages/index.tsx
@@ -1,15 +1,7 @@
-import { Fragment } from 'react'
 import { NextPage } from 'next'
 
-import Button from '../components/UI/Button'
-
 const App: NextPage = () => {
-  return (
-    <Fragment>
-      <h1>Category App</h1>
-      <Button type='submit'>create</Button>
-    </Fragment>
-  )
+  return <h1>Category App</h1>
 }
 
 export default App
