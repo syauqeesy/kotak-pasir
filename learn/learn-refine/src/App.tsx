d116f72d7a7494a7aaa0bbd4b44147efee81f3cd
behoon <syauqeesy@gmail.com>
2022-09-05 09:44:39 +0700

    posts list

--- learn/learn-refine/src/App.tsx
+++ learn/learn-refine/src/App.tsx
@@ -2,11 +2,19 @@ import { Refine } from "@pankod/refine-core";
 import routerProvider from "@pankod/refine-react-router-v6";
 import dataProvider from "@pankod/refine-simple-rest";
 
+import { Layout } from "components/Layout"
+import { PostList } from "pages/posts";
+
+import './index.css'
+// import { PostIcon } from "icons"
+
 const App: React.FC = () => {
     return (
         <Refine
             routerProvider={routerProvider}
             dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+            resources={[{ name: "posts", list: PostList }]}
+            Layout={Layout}
         />
     );
 };
